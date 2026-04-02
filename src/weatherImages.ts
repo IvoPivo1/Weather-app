09ba8763c0e4cf0ae75553fb98a3141075ea73a7
IvoPivo1 <ivogjurovski12@gmail.com>
2026-04-02 09:23:13 +0200

    Changed to public images

--- src/weatherImages.ts
+++ src/weatherImages.ts
@@ -1,19 +1,37 @@
+import Clear from "/src/assets/weather/sun.png"
+import Clouds from "/src/assets/weather/clouds.png"
+import Rain from  "/src/assets/weather/rain.png"
+import Drizzle from  "/src/assets/weather/rain.png"
+import Thunderstorm from "/src/assets/weather/storm.png"
+import Snow from "/src/assets/weather/snow.png"
+import Mist from "/src/assets/weather/foggy.png"
+import Fog from "/src/assets/weather/foggy.png"
+import Haze from "/src/assets/weather/foggy.png"
+import Smoke from "/src/assets/weather/foggy.png"
+import Dust from "/src/assets/weather/foggy.png"
+import Sand from "/src/assets/weather/foggy.png"
+import Ash from "/src/assets/weather/foggy.png"
+import Squall from "/src/assets/weather/wind.png"
+import Tornado from "/src/assets/weather/wind.png"
+
+
+
 export const weatherIcons: Record<string, string> = {
-  Clear: "/src/assets/weather/sun.png",
-  Clouds: "/src/assets/weather/clouds.png",
-  Rain: "/src/assets/weather/rain.png",
-  Drizzle: "/src/assets/weather/rain.png",
-  Thunderstorm: "/src/assets/weather/storm.png",
-  Snow: "/src/assets/weather/snow.png",
-  Mist: "/src/assets/weather/foggy.png",
-  Fog: "/src/assets/weather/foggy.png",
-  Haze: "/src/assets/weather/foggy.png",
-  Smoke: "/src/assets/weather/foggy.png",
-  Dust: "/src/assets/weather/foggy.png",
-  Sand: "/src/assets/weather/foggy.png",
-  Ash: "/src/assets/weather/foggy.png",
-  Squall: "/src/assets/weather/wind.png",
-  Tornado: "/src/assets/weather/wind.png",
+  Clear,
+  Clouds,
+  Rain,
+  Drizzle,
+  Thunderstorm,
+  Snow,
+  Mist,
+  Fog,
+  Haze,
+  Smoke,
+  Dust,
+  Sand,
+  Ash,
+  Squall,
+  Tornado,
 
   "few clouds": "/src/assets/weather/few-clouds.png",
   "scattered clouds": "/src/assets/weather/few-clouds.png",
